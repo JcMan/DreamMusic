222aead947b7fc662d2f5670e429e90e6054deb6
JcMan <pjc1993pjc@126.com>
2015-08-03 19:37:23 +0800

    修改了歌手背景下载显示的布局，自适应屏幕大小显示

--- DreamMusic/app/src/main/java/dream/app/com/dreammusic/util/Messageutil.java
+++
@@ -1,8 +0,0 @@
-package dream.app.com.dreammusic.util;
-
-/**
- * Created by Administrator on 2015/7/10.
- */
-public class Messageutil {
-    public static final int MESSAGE_GO_LOCALMUSICFRAGMENT = 1;
-}
--- DreamMusic/app/src/main/java/dream/app/com/dreammusic/util/TextUtil.java
+++
@@ -1,8 +0,0 @@
-package dream.app.com.dreammusic.util;
-
-/**
- * Created by Administrator on 2015/7/5.
- */
-public class TextUtil {
-    public static final String LOADINGNOW = "加载中···";
-}
