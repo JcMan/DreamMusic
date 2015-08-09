4495688f7a5d956013f799f107ec581cf997a810
JcMan <pjc1993pjc@126.com>
2015-08-09 13:37:38 +0800

    小说分类界面的初搭建

--- app/src/main/java/dream/app/com/dreammusic/ui/activity/NovelActivity.java
+++ app/src/main/java/dream/app/com/dreammusic/ui/activity/NovelActivity.java
@@ -1,18 +1,25 @@
 package dream.app.com.dreammusic.ui.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.app.tool.logger.Logger;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import dream.app.com.dreammusic.R;
 import dream.app.com.dreammusic.adapter.MyViewPagerAdapter;
 import dream.app.com.dreammusic.service.MusicService;
+import dream.app.com.dreammusic.ui.view.FlowLayout;
 import dream.app.com.dreammusic.ui.view.MyViewPagerIndicator;
+import dream.app.com.dreammusic.util.MyHttpUtil;
 
 /**
  * Created by Administrator on 2015/8/8.
@@ -23,7 +30,7 @@ public class NovelActivity extends BaseActivity{
     private ViewPager mViewPager;
     private List<View> mViewsList;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novel);
         MusicService.addActivity(this);
@@ -33,7 +40,7 @@ public class NovelActivity extends BaseActivity{
     }
 
     @Override
-    public void initView() {
+    public void initView(){
         super.initView();
         mIndicator = (MyViewPagerIndicator) findViewById(R.id.indicator_novel);
         mViewPager = (ViewPager) findViewById(R.id.viewpager_novel);
@@ -41,7 +48,7 @@ public class NovelActivity extends BaseActivity{
         initIndicator();
     }
 
-    private void initIndicator() {
+    private void initIndicator(){
         mIndicator.setVisibleTabCount(3);
         String titles[] = {"书架","分类","搜索"};
         List<String> _List = Arrays.asList(titles);
@@ -53,12 +60,24 @@ public class NovelActivity extends BaseActivity{
         mViewsList = new ArrayList<View>();
         TextView tv = new TextView(this);
         tv.setText("书架");
-        TextView tv1 = new TextView(this);
-        tv1.setText("分类");
+        View v_classification = View.inflate(this,R.layout.view_novel_classification,null);
+        FlowLayout flowLayout = (FlowLayout) v_classification.findViewById(R.id.flowlayout_novel_classification);
+        String[] novel_classes = getResources().getStringArray(R.array.novel_kind_name);
+        for (int i = 0; i <novel_classes.length ; i++){
+            TextView tv_label = new TextView(this);
+            ViewGroup.MarginLayoutParams params = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(4,4,4,4);
+            tv_label.setTextColor(Color.parseColor("#ffffff"));
+            tv_label.setLayoutParams(params);
+            tv_label.setBackgroundResource(R.drawable.tv_novel_classes);
+            tv_label.setText(novel_classes[i]);
+            flowLayout.addView(tv_label);
+        }
         TextView tv2 = new TextView(this);
         tv2.setText("搜索");
         mViewsList.add(tv);
-        mViewsList.add(tv1);
+        mViewsList.add(v_classification);
         mViewsList.add(tv2);
         mViewPager.setAdapter(new MyViewPagerAdapter(this,mViewsList));
     }
